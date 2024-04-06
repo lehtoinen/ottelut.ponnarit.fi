b04c4cc7bd01829e208387d27588fcf1b27a564b
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2024-04-06 16:15:37 +0300

    Fix site root folder (#10)
    * fix: conf Astro base folder
    Needed to display asssets correctly in GH Pages
    
    * fix: add base to favicon path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+const {
+  ASTRO_BASE,
+  ASTRO_SITE,
+} = import.meta.env;
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: ASTRO_SITE,
+  base: ASTRO_BASE,
+});
