6402d014d68ba8b6531dd801018446380964bb1d
Anssi Lehtonen <lehtovaaralainen@gmail.com>
2024-04-06 16:37:49 +0300

    Hardcode Astro base and site (#13)
    * fix: hardcode astro base and site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-import { loadEnv } from "vite";
 import { defineConfig } from 'astro/config';
-
-const { ASTRO_BASE, ASTRO_SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  site: ASTRO_SITE,
-  base: ASTRO_BASE,
-});
+  site: 'https://lehtoinen.github.io',
+  base: 'ottelut.ponnarit.fi',
+})
