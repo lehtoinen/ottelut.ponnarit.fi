e9e5141432901af5fb2976cf4ad5bad4b1ed702d
lehtoinen <lehtovaaralainen@gmail.com>
2024-04-06 16:23:59 +0300

    fix: use loadEnv to access env vars in Astron config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
+import { loadEnv } from "vite";
 import { defineConfig } from 'astro/config';
 
-const {
-  ASTRO_BASE,
-  ASTRO_SITE,
-} = import.meta.env;
+const { ASTRO_BASE, ASTRO_SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
